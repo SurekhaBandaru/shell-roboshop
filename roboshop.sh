d8b8ce010d3af4cb02e0de303c77aedb4fa03012
Surekha <surekhagidithuri@gmail.com>
2025-05-23 21:04:14 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -7,8 +7,7 @@ DOMAIN_NAME="devopspract.site"
 
 for instance in ${INSTANCES[@]}; do
     #to get the instance id
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-077769cfadde11fb1 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]"
-        --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-077769cfadde11fb1 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
 
     if [ $instance != "frontend" ]; then #query private if not frontend
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
