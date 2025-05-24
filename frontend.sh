75b9b8dc5673c7139262c060f89d35a0dddf551b
Surekha <surekhagidithuri@gmail.com>
2025-05-24 23:44:31 +0530

    roboshop app files

--- frontend.sh
+++ frontend.sh
@@ -16,7 +16,7 @@ mkdir -p $LOG_FOLDER
 echo "Script started executing at : $(date)" | tee -a $LOG_FILE
 
 #check if user has root access
-if [ $USERID -ne 0]; then
+if [ $USERID -ne 0 ]; then
     echo -e "$R ERROR: Please run the command with root access $N" | tee -a $LOG_FILE
     # as there is no root access, exit the process here
     exit 1
@@ -25,7 +25,7 @@ else
 fi
 
 #create log directiry
-mkdir -p $LOG_FILE
+#mkdir -p $LOG_FILE
 
 VALIDATE() {
     if [ $1 -eq 0 ]; then
