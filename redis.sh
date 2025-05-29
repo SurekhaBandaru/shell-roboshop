2fdef41156f122bba81e44ca952a45391c08ae19
Surekha <surekhagidithuri@gmail.com>
2025-05-29 22:12:02 +0530

    roboshop

---
+++ redis.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+START_TIME=$(date+ %s)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOG_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOG_FOLDER
+echo "Script started executing at : $(date)" | tee -a $LOG_FILE
+
+if [ USERID -ne 0 ]; then
+    echo -e "$R ERROR:: You are not running with root access $N" | tee -a $LOG_FILE
+else
+    echo -e "$Y You are running with root access $N" | tee -a $LOG_FILE
+fi
+
+VALIDATE() {
+    if [ $1 -eq 0 ]; then
+        echo -e "$2 is .... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ..... $R FAILURE $N" | tee -a $LOG_FILE
+    fi
+}
+
+#disable default version
+dnf module disable redis -y $>>$LOG_FILE
+VALIDATE $? "Disabling redis default version"
+
+#enable default version
+dnf module enable redis:7 -y $>>$LOG_FILE
+VALIDATE $? "Enabling redis 7"
+
+#install redis
+dnf install redis -y $>>$LOG_FILE
+VALIDATE $? "Installing redis 7"
+
+#change 127.0.0.1 to internet accessible 0.0.0.0
+#protectedmode to no
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e 'protected-mode/c protected-mode no' /etc/redis/redis.conf
+
+systemctl enable redis $>>$LOG_FILE
+VALIDATE $? "Enabling redis"
+
+systemctl start redis $>>$LOG_FILE
+VALIDATE $? "Starting redis"
+
+# %s is to display date in seconds format
+END_TIME=$(date+ %s)
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+echo -e "Script Execution completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+
---
+++ user.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+START_TIME=$(date+ %s)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOG_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOG_FOLDER
+echo "Script started executing at : $(date)" | tee -a $LOG_FILE
+
+if [ USERID -ne 0 ]; then
+    echo -e "$R ERROR:: You are not running with root access $N" | tee -a $LOG_FILE
+else
+    echo -e "$Y You are running with root access $N" | tee -a $LOG_FILE
+fi
+
+VALIDATE() {
+    if [ $1 -eq 0 ]; then
+        echo -e "$2 is .... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ..... $R FAILURE $N" | tee -a $LOG_FILE
+    fi
+}
+
+dnf module diable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disabling nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enabling Nodejs 20"
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing nodejs version 20"
+
+#check if user already created
+id roboshop
+
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell sbin/nologin --comment "Roboshop System User" roboshop
+    VALIDATE $? "Creating System user Roboshop"
+else
+    echo -e "User roboshop already created... $Y SKIPPING $N"
+fi
+
+mkdir -p /app
+VALIDATE $? "Creating App directory"
+
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>>$LOG_FILE
+VALIDATE $? "Dowloading user service code to tmp/uzer.zip folder"
+
+cd /app
+rm -rf /app/*
+VALIDATE $? "Removing content in App folder"
+
+unzip /tmp/user.zip &>>$LOG_FILE
+VALIDATE $? "Unzipping user service"
+
+npm install &>>LOG_FILE
+#vim /systemd/system/user.service
+cp $SCRIPT_DIR/user.service /systemd/system/user.service &>>LOG_FILE
+VALIDATE $? "Copying user service info to systemd folder"
+
+systemctl daemon-reload &>>LOG_FILE
+VALIDATE $? "Daemon reload- reloading systemd folder adter change"
+
+systemctl enable user.service 
+$VALIDATE $? "Enabling User Service"
+
+systemctl start user.service $>>$LOG_FILE
+VALIDATE $? "Disabling user service"a
